f504625e07eaeee3c4d295741224188a1a3ae58a
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-03-31 22:27:30 +0530

    Few Changes

---
+++ .NET6MVC/a1-eRestaurant/Services/eRestaurant.Services.ProductAPI/DbContexts/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using eRestaurant.Services.ProductAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace eRestaurant.Services.ProductAPI.DbContexts
+{
+
+    public class ApplicationDbContext : DbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product>? Products { get; set; }
+    }
+
+}
