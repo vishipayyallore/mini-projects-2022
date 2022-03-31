f11c683651bb319287d0e2c11abb9b0406382879
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-03-31 23:03:11 +0530

    Dtos

---
+++ .NET6MVC/a1-eRestaurant/Services/eRestaurant.Services.ProductAPI/Dtos/ResponseDto.cs
@@ -0,0 +1,15 @@
+﻿namespace eRestaurant.Services.ProductAPI.Dtos
+{
+    
+    public class ResponseDto
+    {
+        public bool? IsSuccess { get; set; } = true;
+        
+        public object? Result { get; set; }
+        
+        public string? DisplayMessage { get; set; } = string.Empty;
+        
+        public List<string>? ErrorMessages { get; set; }
+    }
+
+}
