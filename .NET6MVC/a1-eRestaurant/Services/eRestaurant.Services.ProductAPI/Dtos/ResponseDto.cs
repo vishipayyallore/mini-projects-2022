86022a7ffdee57b4f84ab3850d904f94528e5661
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-05-04 15:48:17 +0530

    Update ResponseDto.cs

--- .NET6MVC/a1-eRestaurant/Services/eRestaurant.Services.ProductAPI/Dtos/ResponseDto.cs
+++ .NET6MVC/a1-eRestaurant/Services/eRestaurant.Services.ProductAPI/Dtos/ResponseDto.cs
@@ -1,14 +1,14 @@
 ﻿namespace eRestaurant.Services.ProductAPI.Dtos
 {
-    
+
     public class ResponseDto
     {
         public bool? IsSuccess { get; set; } = true;
-        
+
         public object? Result { get; set; }
-        
+
         public string? DisplayMessage { get; set; } = string.Empty;
-        
+
         public List<string>? ErrorMessages { get; set; }
     }
 
