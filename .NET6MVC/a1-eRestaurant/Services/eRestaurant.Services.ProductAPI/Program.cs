913ca47296c967db4418db159a6d7afeca4a6d72
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-05-09 22:38:54 +0530

    Auto Mapper

--- .NET6MVC/a1-eRestaurant/Services/eRestaurant.Services.ProductAPI/Program.cs
+++ .NET6MVC/a1-eRestaurant/Services/eRestaurant.Services.ProductAPI/Program.cs
@@ -1,3 +1,5 @@
+using AutoMapper;
+using eRestaurant.Services.ProductAPI.Configuration;
 using eRestaurant.Services.ProductAPI.DbContexts;
 using Microsoft.EntityFrameworkCore;
 
@@ -6,6 +8,10 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+IMapper mapper = MappingConfig.RegisterMaps().CreateMapper();
+builder.Services.AddSingleton(mapper);
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
--- .NET6MVC/a1-eRestaurant/Services/eRestaurant.Services.ProductAPI/Repositories/IProductRepository.cs
+++ .NET6MVC/a1-eRestaurant/Services/eRestaurant.Services.ProductAPI/Repositories/IProductRepository.cs
@@ -1,8 +1,17 @@
-﻿namespace eRestaurant.Services.ProductAPI.Repositories
+﻿using eRestaurant.Services.ProductAPI.Dtos;
+
+namespace eRestaurant.Services.ProductAPI.Repositories
 {
 
     public interface IProductRepository
     {
+        Task<IEnumerable<ProductDto>> GetProducts();
+
+        Task<ProductDto> GetProductById(int productId);
+
+        Task<ProductDto> CreateUpdateProduct(ProductDto productDto);
+
+        Task<bool> DeleteProduct(int productId);
     }
 
 }
