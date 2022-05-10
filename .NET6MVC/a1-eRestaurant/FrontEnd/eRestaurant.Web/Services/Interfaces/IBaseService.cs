f87af250a6416107ee2fff54ae7d2131d6ec6c11
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-05-10 11:18:51 +0530

    Few Changes

---
+++ .NET6MVC/a1-eRestaurant/FrontEnd/eRestaurant.Web/Services/Interfaces/IBaseService.cs
@@ -0,0 +1,13 @@
+﻿using eRestaurant.Web.Models;
+
+namespace eRestaurant.Web.Services.Interfaces
+{
+
+    public interface IBaseService
+    {
+        ResponseDto ResponseModel { get; set; }
+
+        Task<T> SendAsync<T>(ApiRequest apiRequest);
+    }
+
+}
---
+++ .NET6MVC/a1-eRestaurant/FrontEnd/eRestaurant.Web/Services/Interfaces/IProductsService.cs
@@ -0,0 +1,19 @@
+﻿using eRestaurant.Web.Models;
+
+namespace eRestaurant.Web.Services.Interfaces
+{
+
+    public interface IProductsService : IBaseService
+    {
+        Task<T> GetAllProductsAsync<T>(string token);
+
+        Task<T> GetProductByIdAsync<T>(int id, string token);
+
+        Task<T> CreateProductAsync<T>(ProductDto productDto, string token);
+
+        Task<T> UpdateProductAsync<T>(ProductDto productDto, string token);
+
+        Task<T> DeleteProductAsync<T>(int id, string token);
+    }
+
+}
