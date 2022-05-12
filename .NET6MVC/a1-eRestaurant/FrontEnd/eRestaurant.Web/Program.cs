bcce2d54e18b3dc7ed02e2780e7e9a1364e5736f
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-05-12 14:04:34 +0530

    Few Changes

--- .NET6MVC/a1-eRestaurant/FrontEnd/eRestaurant.Web/Program.cs
+++ .NET6MVC/a1-eRestaurant/FrontEnd/eRestaurant.Web/Program.cs
@@ -1,6 +1,19 @@
+using eRestaurant.Web.Common;
+using eRestaurant.Web.Services;
+using eRestaurant.Web.Services.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+// Base Urls
+Constants.ProductAPIBase = builder.Configuration["ServiceUrls:ProductAPI"];
+
+// Application Dependencies
+builder.Services.AddHttpClient<IProductsService, ProductService>();
+
+// Services
+builder.Services.AddScoped<IProductsService, ProductService>();
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
