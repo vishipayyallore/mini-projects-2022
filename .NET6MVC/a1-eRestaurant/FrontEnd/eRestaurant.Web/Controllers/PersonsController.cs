13733a978c9a6fa7a53fb4a826009dafbacd5172
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-05-14 13:00:41 +0530

    Teja - 1

---
+++ .NET6MVC/a1-eRestaurant/FrontEnd/eRestaurant.Web/Controllers/PersonsController.cs
@@ -0,0 +1,26 @@
+﻿using eRestaurant.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eRestaurant.Web.Controllers
+{
+
+    public class PersonsController : Controller
+    {
+        public IActionResult PersonsIndex()
+        {
+            IEnumerable<PersonModel> persons = GetDummyPersons();
+
+            return View(persons);
+        }
+
+        private static IEnumerable<PersonModel> GetDummyPersons()
+        {
+            return new List<PersonModel>
+            {
+                new PersonModel { PersonId = 1, Name = "Shiva", Gender = "Male", City = "Hyderabad" },
+                new PersonModel { PersonId = 2, Name = "Manish", Gender = "Male", City = "Jaipur" }
+            };
+        }
+    }
+
+}
