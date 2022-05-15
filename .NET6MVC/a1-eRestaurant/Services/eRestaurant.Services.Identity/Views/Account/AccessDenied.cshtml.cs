4c96715a16dd6d994b4dec64f383c69c3fc35808
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-05-15 19:58:15 +0530

    Added Iden UI Files

---
+++ .NET6MVC/a1-eRestaurant/Services/eRestaurant.Services.Identity/Views/Account/AccessDenied.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace eshop_microservices.Pages.Account;
+
+public class AccessDeniedModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
---
+++ .NET6MVC/a1-eRestaurant/Services/eRestaurant.Services.Identity/Views/Account/Logout/LogoutOptions.cs
@@ -0,0 +1,8 @@
+
+namespace eshop_microservices.Pages.Logout;
+
+public class LogoutOptions
+{
+    public static bool ShowLogoutPrompt = true;
+    public static bool AutomaticRedirectAfterSignOut = false;
+}
