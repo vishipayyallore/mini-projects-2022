c37f2c4ed64fc84ea059fb84c94f83786dadb7bb
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-05-16 17:59:03 +0530

    Few Changes

--- .NET6MVC/a1-eRestaurant/Services/eRestaurant.Services.Identity/Models/ApplicationUser.cs
+++ .NET6MVC/a1-eRestaurant/Services/eRestaurant.Services.Identity/Models/ApplicationUser.cs
@@ -1,7 +1,6 @@
 ﻿// Copyright (c) Duende Software. All rights reserved.
 // See LICENSE in the project root for license information.
 
-
 using Microsoft.AspNetCore.Identity;
 
 namespace eRestaurant.Services.Identity.Models
@@ -9,5 +8,9 @@ namespace eRestaurant.Services.Identity.Models
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
     }
+
 }
