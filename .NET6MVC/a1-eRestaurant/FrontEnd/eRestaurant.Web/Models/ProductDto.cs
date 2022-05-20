3bfe5294b929cf9c138070f5cb21d5d24b196bed
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-05-21 00:57:08 +0530

    Details Verified

--- .NET6MVC/a1-eRestaurant/FrontEnd/eRestaurant.Web/Models/ProductDto.cs
+++ .NET6MVC/a1-eRestaurant/FrontEnd/eRestaurant.Web/Models/ProductDto.cs
@@ -1,8 +1,16 @@
-﻿namespace eRestaurant.Web.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eRestaurant.Web.Models
 {
 
     public class ProductDto
     {
+
+        public ProductDto()
+        {
+            Count = 1;
+        }
+
         public int ProductId { get; set; }
 
         public string Name { get; set; } = string.Empty;
@@ -14,6 +22,9 @@
         public string CategoryName { get; set; } = string.Empty;
 
         public string ImageUrl { get; set; } = string.Empty;
+
+        [Range(1, 10)]
+        public int Count { get; set; }
     }
 
 }
