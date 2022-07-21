fbdeb1a8659a3cc944e29b6e56985ad36d50862d
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-07-21 23:33:49 +0530

    Data Seeding

--- BlazorDemos/CarRentalManagement/CarRentalManagement/Server/Data/ApplicationDbContext.cs
+++ BlazorDemos/CarRentalManagement/CarRentalManagement/Server/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using CarRentalManagement.Server.Models;
+﻿using CarRentalManagement.Server.Configurations.Entities;
+using CarRentalManagement.Server.Models;
 using CarRentalManagement.Shared.Domain;
 using Duende.IdentityServer.EntityFramework.Options;
 using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
@@ -26,6 +27,19 @@ namespace CarRentalManagement.Server.Data
         public DbSet<Customer> Customers { get; set; }
 
         public DbSet<Booking> Bookings { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.ApplyConfiguration(new ColourSeedConfiguration());
+            builder.ApplyConfiguration(new MakeSeedConfiguration());
+            builder.ApplyConfiguration(new ModelSeedConfiguration());
+            builder.ApplyConfiguration(new RoleSeedConfiguration());
+            builder.ApplyConfiguration(new UserSeedConfiguration());
+            builder.ApplyConfiguration(new UserRoleSeedConfiguration());
+        }
+
     }
 
 }
