5b126b3173dbee1928a601a54a7e00889abf30e7
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-08-26 00:02:24 +0530

    ..

---
+++ A3-eShop/Source/eShop.ProductsAPI/Services/ProductsService.cs
@@ -0,0 +1,13 @@
+﻿using eShop.Infrastructure.Command.Product;
+using eShop.Infrastructure.Event.Product;
+
+namespace eShop.ProductsAPI.Services
+{
+    public class ProductsService : IProductsService
+    {
+        public Task<ProductCreated> AddProduct(CreateProduct createProduct)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
