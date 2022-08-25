d289fad9db6e2f50c564854f2842594bbeebabe9
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-08-25 11:02:02 +0530

    Added Product Command and Event

---
+++ A3-eShop/Source/eShop.Infrastructure/Command/Product/CreateProduct.cs
@@ -0,0 +1,16 @@
+﻿namespace eShop.Infrastructure.Command.Product
+{
+    public class CreateProduct
+    {
+        public Guid ProductId { get; set; }
+
+        public string ProductName { get; set; } = string.Empty;
+
+        public string ProductDescription { get; set; } = string.Empty;
+
+        public float ProductPrice { get; set; }
+
+        public Guid CategoryId { get; set; }
+    }
+
+}
---
+++ A3-eShop/Source/eShop.Infrastructure/Event/Product/ProductCreated.cs
@@ -0,0 +1,12 @@
+﻿namespace eShop.Infrastructure.Event.Product
+{
+    public class ProductCreated
+    {
+        public Guid ProductId { get; set; }
+
+        public string ProductName { get; set; } = string.Empty;
+
+        public DateTime CreatedAt { get; set; }
+    }
+
+}
