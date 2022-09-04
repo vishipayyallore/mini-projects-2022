58b616abb953a92a8d69e90ce95499b1a4db5807
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-09-04 13:31:45 +0530

    Model Classes

---
+++ A5-HPlusSport/Source/HPlusSport.API/Models/User.cs
@@ -0,0 +1,13 @@
+﻿namespace HPlusSport.API.Models
+{
+    
+    public class User
+    {
+        public int Guid { get; set; }
+        
+        public string Email { get; set; }
+        
+        public virtual List<Order> Orders { get; set; }
+    }
+
+}
