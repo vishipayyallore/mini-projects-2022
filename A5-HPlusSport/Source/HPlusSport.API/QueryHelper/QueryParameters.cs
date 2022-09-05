47be4c68534066aa85d513ccc8bf0b6e56ff7660
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-09-05 12:45:04 +0530

    Paging completed

---
+++ A5-HPlusSport/Source/HPlusSport.API/QueryHelper/QueryParameters.cs
@@ -0,0 +1,26 @@
+﻿namespace HPlusSport.API.QueryHelper
+{
+
+    public class QueryParameters
+    {
+        public int Page { get; set; }
+
+        const int _maxSize = 20;
+
+        private int _size = 10;
+
+        public int Size
+        {
+            get
+            {
+                return _size;
+            }
+
+            set
+            {
+                _size = Math.Min(_maxSize, value);
+            }
+        }
+    }
+
+}
