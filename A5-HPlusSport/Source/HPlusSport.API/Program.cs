afe6661c98e2da578abcc4bb0b3e3ef5deac57f7
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-10-30 23:19:29 +0530

    Add CORS to Program.cs

--- A5-HPlusSport/Source/HPlusSport.API/Program.cs
+++ A5-HPlusSport/Source/HPlusSport.API/Program.cs
@@ -8,6 +8,17 @@ builder.Services.AddDbContext<ShopDbContext>(options =>
                 options.UseInMemoryDatabase("Shop"));
 
 builder.Services.AddControllers();
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(builder =>
+    {
+        builder.WithOrigins(new string[] { "http://localhost:4200" })
+               .AllowAnyHeader()
+               .AllowAnyMethod();
+    });
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -22,6 +33,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors();
 
 app.UseAuthorization();
 
